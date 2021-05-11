7781121e17a02eb7267e09bec4ec4a1f68a0d527
Rina <rina.furukoshi@force.copr.co.jp>
2021-05-11 11:43:55 +0900

    initial commit

---
+++ JS/jspractice.js
@@ -0,0 +1,25 @@
+// document.write("Hellow World")
+
+// console.log("Hello World" + "田中さん")
+// console.log(10+10)
+// console.log("10"+"10")
+
+// let x = 1;
+// let y = 1;
+// x++;
+// console.log(x)
+// y--;
+// console.log(y)
+
+// let x = 7;
+// let y = x++;
+
+// console.log(x);
+// console.log(y);
+
+let age =30;
+const name ="田中太郎"
+
+age = age + 2; 
+console.log(`${name}さんの年齢は${age}です。`);
+
